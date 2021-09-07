67c0163f68c6c3689b0a82f9ae1d46375ec01df3
emege93 <emege93@gmail.com>
2021-09-07 16:23:04 +0200

    add PresentationService

---
+++ src/services/PresentationService.ts
@@ -0,0 +1,65 @@
+import vc from "@sphereon/rn-vc-js";
+import { v4 as uuid } from "uuid";
+
+import { Ed25519VerificationKey2020 } from "@digitalbazaar/ed25519-verification-key-2020";
+import { Ed25519Signature2020 } from "@digitalbazaar/ed25519-signature-2020";
+import { Presentation } from "../interfaces/types";
+
+export default class PresentationService {
+  public static async createPresentation(
+    verifiableCredential: Credential[],
+    id: string,
+    holder: string
+  ) {
+    try {
+      vc.createPresentation({
+        verifiableCredential,
+        id,
+        holder,
+      });
+    } catch (error) {
+      console.error("Create Credential Error", error);
+    }
+  }
+
+  public static async signPresentation(
+    presentation: Presentation,
+    challenge: string
+  ) {
+    const keyPair = await Ed25519VerificationKey2020.generate();
+    const suite = new Ed25519Signature2020({ key: keyPair });
+
+    try {
+      await vc.signPresentation({
+        presentation,
+        suite,
+        challenge,
+      });
+    } catch (error) {
+      console.error("Create Credential Error", error);
+    }
+  }
+
+  public static async verifyPresentation(presentation: Presentation) {
+    const keyPair = await Ed25519VerificationKey2020.generate();
+    const suite = new Ed25519Signature2020({ key: keyPair });
+    const challenge = uuid();
+
+    try {
+      await vc.verify({ presentation, challenge, suite });
+    } catch (error) {
+      console.error("Verify Presentation Error", error);
+    }
+  }
+
+  public static async verifyUnsignedPresentation(presentation: Presentation) {
+    const keyPair = await Ed25519VerificationKey2020.generate();
+    const suite = new Ed25519Signature2020({ key: keyPair });
+
+    try {
+      await vc.verify({ presentation, suite, unsignedPresentation: true });
+    } catch (error) {
+      console.error("Verify Presentation Error", error);
+    }
+  }
+}
