0a8287ed6752adafa8929398e657e5fb8e160922
emege93 <emege93@gmail.com>
2021-09-09 17:55:00 +0200

    add returns

--- src/services/PresentationService.ts
+++ src/services/PresentationService.ts
@@ -3,63 +3,78 @@ import { v4 as uuid } from "uuid";
 
 import { Ed25519VerificationKey2020 } from "@digitalbazaar/ed25519-verification-key-2020";
 import { Ed25519Signature2020 } from "@digitalbazaar/ed25519-signature-2020";
-import { Presentation } from "../interfaces/types";
+import { Presentation, PresentationSigned } from "../interfaces/types";
 
 export default class PresentationService {
   public static async createPresentation(
     verifiableCredential: Credential[],
     id: string,
     holder: string
-  ) {
+  ): Promise<Presentation> {
     try {
-      vc.createPresentation({
+      const presentationResult: Presentation = await vc.createPresentation({
         verifiableCredential,
         id,
         holder,
       });
+      return presentationResult;
     } catch (error) {
-      console.error("Create Credential Error", error);
+      throw new Error("Create Credential Error");
     }
   }
 
   public static async signPresentation(
     presentation: Presentation,
     challenge: string
-  ) {
+  ): Promise<PresentationSigned | boolean> {
     const keyPair = await Ed25519VerificationKey2020.generate();
     const suite = new Ed25519Signature2020({ key: keyPair });
 
+    if (!keyPair || !suite) return false;
+
     try {
-      await vc.signPresentation({
+      const signedResult: PresentationSigned = await vc.signPresentation({
         presentation,
         suite,
         challenge,
       });
+
+      return signedResult;
     } catch (error) {
-      console.error("Create Credential Error", error);
+      throw new Error("Create Credential Error");
     }
   }
 
-  public static async verifyPresentation(presentation: Presentation) {
+  public static async verifyPresentation(
+    presentation: Presentation
+  ): Promise<boolean> {
     const keyPair = await Ed25519VerificationKey2020.generate();
     const suite = new Ed25519Signature2020({ key: keyPair });
     const challenge = uuid();
 
+    if (!keyPair || !suite) return false;
+
     try {
       await vc.verify({ presentation, challenge, suite });
+      return true;
     } catch (error) {
-      console.error("Verify Presentation Error", error);
+      throw new Error("Verify Presentation Error");
     }
   }
 
-  public static async verifyUnsignedPresentation(presentation: Presentation) {
+  public static async verifyUnsignedPresentation(
+    presentation: Presentation
+  ): Promise<boolean> {
     const keyPair = await Ed25519VerificationKey2020.generate();
     const suite = new Ed25519Signature2020({ key: keyPair });
 
+    if (!keyPair || !suite) return false;
+
     try {
       await vc.verify({ presentation, suite, unsignedPresentation: true });
+      return true;
     } catch (error) {
-      console.error("Verify Unsigned Presentation Error", error);
+      throw new Error("Verify Unsigned Presentation Error");
     }
   }
 }
