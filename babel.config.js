a8a12f0a194686b1d3d0c37fbbd3ba802c24018b
emege93 <emege93@gmail.com>
2021-09-29 17:07:59 +0200

    add configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  presets: [
+    ['module:metro-react-native-babel-preset'],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          src: './src'
+        }
+      }
+    ],
+    'babel-plugin-transform-typescript-metadata'
+  ]
 }
